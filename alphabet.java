c20e92c64cc52ffb76be67e466b686195865f807
S Mirunalini <mirunalinirajan@gmail.com>
2016-08-10 21:47:21 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class alphabet
+{
+	public static void main(String [] args)
+	{
+		Scanner S=new Scanner(System.in);
+		System.out.println("Enter One character to check");
+		String s=S.next();
+		char[] ss=s.toCharArray();
+		if(Character.isAlphabetic(ss[0]))
+			System.out.println("Alphabet");
+		else
+			System.out.println("Not a Alphabet");
+	}
+}
