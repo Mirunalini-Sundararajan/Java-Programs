d1b5dd02c4453325f910c4cd22d5ed724c4115a8
S Mirunalini <mirunalinirajan@gmail.com>
2016-08-10 22:40:49 +0530

    Add files via upload

---
+++ naturalnos.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class naturalnos
+{
+	public static void main(String [] args)
+	{
+		Scanner S=new Scanner(System.in);
+		int sum=0;
+		System.out.println("Enter hte Range of the Natural numbers to be added");
+		int n=S.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			sum+=i;
+		}
+		System.out.println("The sum of "+n+" natural numbers is "+ sum);
+	}
+}
