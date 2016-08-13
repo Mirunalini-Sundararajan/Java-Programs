b994bd7dbcbf8c19254f83c3ede3e9fbbb9afe01
S Mirunalini <mirunalinirajan@gmail.com>
2016-08-14 00:01:27 +0530

    Add files via upload

---
+++ naturalnos.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class naturalnos
+{
+	public static void main(String [] args)
+	{
+		Scanner S=new Scanner(System.in);
+		int sum=0;
+		System.out.println("Enter the N value");
+		int N=S.nextInt();
+		for(int i=1;i<=N;i++)
+		{
+			sum+=i;
+		}
+		System.out.println("Sum = "+ sum);
+	}
+}
