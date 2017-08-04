7d082f030f46b3f61e7388e7f8c12cbf2acbe68c
S Mirunalini <mirunalinirajan@gmail.com>
2017-08-04 21:58:22 +0530

    Add files via upload

--- posneg.java
+++ posneg.java
@@ -7,21 +7,21 @@ public class posneg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner S=new Scanner(System.in);
+		System.out.println("Enter any number");
 		int n=S.nextInt();
-		if(n!=0)
+		if(n>0)
 		{
-			if(n>0)
-			{
-				System.out.println("Positive");
-			}
-			else
-			{
-				System.out.println("Negative");
-			}
+			System.out.println("Given number is a Positive number");
+		}
+		else if(n==0)
+		{
+			System.out.println("Given number is zero");
 		}
 		else
 		{
-			System.out.println("Zero");
+			System.out.println("Given no is a negative number");
 		}
+
 	}
+
 }
